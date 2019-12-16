e467c9bc0a017932fbfb3f4d99c3d03d3fd229e6
nikolbashirsa <58791481+nikolbashirsa@users.noreply.github.com>
2019-12-16 21:03:26 +0200

    Add files via upload

--- SleepCommand.cpp
+++ SleepCommand.cpp
@@ -2,13 +2,16 @@
 #include "Command.h"
 #include <string>
 #include <unistd.h>
+#include <chrono>
+#include <thread>
 
 //
 // Created by nikol on 14/12/2019.
 //
 int SleepCommand::execute(string *s) {
     string check = *s;
-    usleep(stoi(*s));
+    int sleep_time = stoi(*s);
+    this_thread::sleep_for(chrono::milliseconds(sleep_time));
     // jump 2 because of the word sleep and the number
     return 2;
 }
